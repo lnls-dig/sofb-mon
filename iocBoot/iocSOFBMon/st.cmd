9128d409218bd631f6526cd1c73f428f88fcd637
Érico Nogueira <erico.rolim@lnls.br>
2023-10-24 10:53:07 -0300

    SOFBMonApp: parametrize PV names.

--- iocBoot/iocSOFBMon/st.cmd
+++ iocBoot/iocSOFBMon/st.cmd
@@ -10,10 +10,10 @@ dbLoadDatabase "../../dbd/SOFBMon.dbd"
 SOFBMon_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/SOFBMonOrbit.db", "P=SI-Glob:, R=AP-SOFB:")
+dbLoadRecords("../../db/SOFBMonOrbit.db", "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowOrb")
 dbLoadRecords("../../db/SOFBMonOrbitConfig.db", "P=SI-Glob:, R=AP-SOFB:")
 
 iocInit()
 
 ## Start any sequence programs
-seq SOFBMonOrbit, "P=SI-Glob:, R=AP-SOFB:"
+seq SOFBMonOrbit, "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowOrb, FIRST=PosX-Mon, SECOND=PosY-Mon"
