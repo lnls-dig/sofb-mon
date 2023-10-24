a6219197e78d5c623c37f3aedfa7c12cf4016b97
Érico Nogueira <erico.rolim@lnls.br>
2023-10-24 10:53:48 -0300

    iocSOFBMon: instantiate SlowSumQ.
    Since the Sum and PosQ values change in the same way PosX and PosY do
    (i.e. synchronized by the monit rate), we can form "orbits" for them.

--- iocBoot/iocSOFBMon/st.cmd
+++ iocBoot/iocSOFBMon/st.cmd
@@ -11,9 +11,11 @@ SOFBMon_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
 dbLoadRecords("../../db/SOFBMonOrbit.db", "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowOrb")
+dbLoadRecords("../../db/SOFBMonOrbit.db", "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowSumQ")
 dbLoadRecords("../../db/SOFBMonOrbitConfig.db", "P=SI-Glob:, R=AP-SOFB:")
 
 iocInit()
 
 ## Start any sequence programs
 seq SOFBMonOrbit, "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowOrb, FIRST=PosX-Mon, SECOND=PosY-Mon"
+seq SOFBMonOrbit, "P=SI-Glob:, R=AP-SOFB:, TARGET=SlowSumQ, FIRST=Sum-Mon, SECOND=PosQ-Mon"
